e06d8534087eaa640d80ba156516543a11a07234
Ikram Eddahby <ikram22eddahby@gmail.com>
2025-07-24 23:24:13 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/homepage/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
